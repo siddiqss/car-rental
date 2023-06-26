e54ae1a4a4bc19142b5b7b98c57bd8fcc4e6b01f
talha <tstalhasiddique@gmail.com>
2023-06-27 04:07:33 +0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import { Footer, NavBar } from "@components";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Drive Flex",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className='relative'>
         <NavBar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
